11e40c614bee34f16d377dd8aaedaecfde2796ec
renato.hossotani <renato.hossotani@indigosoft.com.br>
2021-06-09 15:23:33 -0300

    Pagina listagem

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
 
 # Create your views here.
+
+#def index(request):
+#    return redirect('/agenda/')
+
+def lista_eventos(request):
+    usuario = request.user
+    #evento = Evento.objects.get(id=1)
+    evento = Evento.objects.all()
+    #evento = Evento.objects.filter(usuario=usuario)
+    data = {'eventos':evento}
+    return render(request, 'agenda.html', data)
